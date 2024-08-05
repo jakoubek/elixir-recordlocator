de601244a0fe2eb8a3c6450f32f45f87a0855a30
Oliver Jakoubek <mail@oliverjakoubek.de>
2024-08-05 12:55:56 +0200

    Enhanced mix.exs for publishing on hex.pm

--- mix.exs
+++ mix.exs
@@ -1,28 +1,47 @@
 defmodule Recordlocator.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/jakoubek/elixir-recordlocator"
+  @version "1.0.0"
+
   def project do
     [
       app: :recordlocator,
-      version: "0.1.0",
+      version: @version,
+      description: description(),
+      package: package(),
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp description do
+    "Module for encoding integers to Recordlocators and decoding Recordlocators back to integers."
+  end
+
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      maintainers: ["Oliver Jakoubek"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp deps do
+    [{:ex_doc, "~> 0.31", only: :dev, runtime: false}]
+  end
+
+  defp docs do
+    [
+      main: "readme"
     ]
   end
 end
